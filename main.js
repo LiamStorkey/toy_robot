5eac4f5acb4364422dbdf6142bddb0149aa3ae2a
Liam Storkey <storkey.liam@gmail.com>
2018-11-05 07:47:15 +1100

    FEAT: added command line interface

--- main.js
+++ main.js
@@ -1,14 +1,57 @@
+const readline = require('readline');
 var Robot = require('./Robot');
-var Table = require('./Table');
 
 var robot = new Robot();
 
-robot.Place(0,0,'NORTH');
-robot.Move();
-robot.ChangeFace('LEFT');
-robot.Move();
-robot.ChangeFace('LEFT');
-robot.Move();
-robot.ChangeFace('LEFT');
-robot.Move();
-robot.Report();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt('Welcome to the Toy Robot simulator, start sending commands to the robot (PLACE, LEFT, RIGHT, MOVE, REPORT, EXIT)\n');
+rl.prompt(true);
+
+rl.on('line', function(line) {
+  if (!line) return;
+
+  let input = line.toUpperCase();
+  let parms = input.split(' ')[1];
+  let command = input.split(' ')[0];
+
+
+  switch(command){
+    case('EXIT'):
+      rl.close();
+      break;
+    case('PLACE'):
+      console.log('PLACE METHOD');
+      break;
+    case('LEFT'):
+      console.log('LEFT METHOD');
+      break;
+    case('RIGHT'):
+      console.log('RIGHT METHOD');
+      break;
+    case('MOVE'):
+      console.log('MOVE METHOD');
+      break;
+    case('REPORT'):
+      console.log('REPORT METHOD');
+      break;
+  }
+})
+.on('close', function() {
+  console.log('Thanks for playing');
+  process.exit(0);
+});
+
+
+// robot.Place(0,0,'NORTH');
+// robot.Move();
+// robot.ChangeFace('LEFT');
+// robot.Move();
+// robot.ChangeFace('LEFT');
+// robot.Move();
+// robot.ChangeFace('LEFT');
+// robot.Move();
+// robot.Report();
